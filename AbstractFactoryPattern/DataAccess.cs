e6d69d94b83dc4648e497c73409f906f174688e8
LambertJi <jiwei@inspur.com>
2022-01-07 18:45:22 +0800

    添加抽象工厂模式示例代码

---
+++ AbstractFactoryPattern/DataAccess.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.Configuration;
+
+namespace AbstractFactoryPattern
+{
+    /// <summary>
+    /// 使用反射+配置文件达到最优
+    /// </summary>
+    internal class DataAccess
+    {
+        private static readonly string AssemblyName = "AbstractFactoryPattern";
+        private static readonly string db = ConfigurationManager.AppSettings["DB"];
+
+        public static IUser CreateUser()
+        {
+            string className = AssemblyName + "." + db + "User";
+            return (IUser)Assembly.Load(AssemblyName).CreateInstance(className);
+        }
+
+        public static IDepartment CreateDepartment()
+        {
+            string className = AssemblyName + "." + db + "Department";
+            return (IDepartment)Assembly.Load(AssemblyName).CreateInstance(className);
+        }
+    }
+
+    /// <summary>
+    /// 使用简单工厂来改进
+    /// </summary>
+    internal class DataAccess_SimpleFactory
+    {
+        private static readonly string db = "Sqlserver";
+        //private static readonly string db = "Access";
+
+        public static IUser CreateUser()
+        {
+            IUser result;
+            switch (db)
+            {
+                case "Sqlserver":
+                    result = new SqlserverUser();
+                    break;
+                case "Access":
+                    result = new AccessUser();
+                    break;
+                default:
+                    result = null;
+                    break;
+            }
+            return result;
+        }
+
+        public static IDepartment CreateDepartment()
+        {
+            IDepartment result;
+            switch (db)
+            {
+                case "Sqlserver":
+                    result = new SqlserverDepartment();
+                    break;
+                case "Access":
+                    result = new AccessDepartment();
+                    break;
+                default:
+                    result = null;
+                    break;
+            }
+            return result;
+        }
+    }
+}
--- AbstractFactoryPattern/Program.cs
+++ AbstractFactoryPattern/Program.cs
@@ -10,6 +10,53 @@ namespace AbstractFactoryPattern
     {
         static void Main(string[] args)
         {
+            #region 使用反射+配置文件达到最优
+            User userA = new User();
+            Department departmentA = new Department();
+
+            IUser iuA = DataAccess.CreateUser();
+            iuA.Insert(userA);
+            iuA.GetUser(1);
+
+            IDepartment ideptA = DataAccess.CreateDepartment();
+            ideptA.Insert(departmentA);
+            ideptA.GetDepartment(1);
+
+            Console.ReadLine();
+            #endregion
+
+            #region 使用简单工厂来改进
+            User userB = new User();
+            Department departmentB = new Department();
+
+            IUser iuB = DataAccess_SimpleFactory.CreateUser();
+            iuB.Insert(userB);
+            iuB.GetUser(1);
+
+            IDepartment ideptB = DataAccess_SimpleFactory.CreateDepartment();
+            ideptB.Insert(departmentB);
+            ideptB.GetDepartment(1);
+
+            Console.ReadLine();
+            #endregion
+
+            #region 抽象工厂
+            User userC = new User();
+            Department departmentC = new Department();
+
+            IFactory factory = new SqlserverFactory();
+            //IFactory factory = new AccessFactory();
+
+            IUser iuC = factory.CreateUser();
+            iuC.Insert(user);
+            iuC.GetUser(1);
+
+            IDepartment ideptC = factory.CreateDepartment();
+            ideptC.Insert(departmentC);
+            ideptC.GetDepartment(1);
+
+            Console.ReadLine();
+            #endregion
         }
     }
 }
